e933f0ccb648010bf7cd7fbb23a68c0b93e072fe
YuliusA <61445406+YuliusA@users.noreply.github.com>
2020-02-25 14:08:41 +0700

    Add files via upload

---
+++ precache-manifest.548d7afa9236fa5d2e775279e2f43715.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb8128fa2c338a32df8ab901c0790abb",
+    "url": "/Fabelio-Product-Filter/index.html"
+  },
+  {
+    "revision": "b064b7bcbfcfe3d72ea9",
+    "url": "/Fabelio-Product-Filter/static/css/main.a71acc3f.chunk.css"
+  },
+  {
+    "revision": "2a585e9ecf0b96db9ff2",
+    "url": "/Fabelio-Product-Filter/static/js/2.c3a23ba8.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/Fabelio-Product-Filter/static/js/2.c3a23ba8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b064b7bcbfcfe3d72ea9",
+    "url": "/Fabelio-Product-Filter/static/js/main.5deb18a5.chunk.js"
+  },
+  {
+    "revision": "c64165d55b0a7e35282d",
+    "url": "/Fabelio-Product-Filter/static/js/runtime-main.c69713c7.js"
+  }
+]);
